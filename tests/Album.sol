18effa0e5c08c69aca4d51fbe89b72d2d8a81e12
siangyeh8818 <siangyeh8818@gmail.com>
2021-11-03 16:54:59 +0800

    feat: add getAlbum & setAlbum func

--- tests/Album.sol
+++ tests/Album.sol
@@ -5,9 +5,26 @@ contract Album {
     string public albumTitle;
     uint public tracks;
     
+    string public constant contractAuthor = 'Jia-Siang Ye';
+    // 常數 ,無法在合約中被修改 ，Compile 會Error
+    
     constructor(){
         artist = 'Siang';
         albumTitle = 'Mine';
         tracks = 8818;
+        contractAuthor;
+    }// 建構子
+    
+    // 返回目前的album 資訊
+    function getAlbum() public view returns (string memory, string memory ,unit) {
+        return (artist, albumTitle ,tracks)
+    }
+
+    //設定 album 資訊
+    function setAlbum(string memory _artist, string memory _albumTitle, unit _tracks) public {
+        artist = artist;
+        albumTitle = albumTitle;
+        tracks = tracks;
+        
     }
 }
